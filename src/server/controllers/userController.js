099adcffda5a0e49a0bbe12d5354b6b6a7fa15c0
bluepaun <bluepaun@gmail.com>
2022-11-12 21:49:50 +0800

    authorize github login 2

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
 export const see = (req, res) => {
   console.log(res.params);
@@ -56,7 +57,7 @@ export const postLogin = async (req, res) => {
     body: { username, password },
   } = req;
   const user = await User.findOne({ username });
-  if (!user) {
+  if (!user || user.socialOnly) {
     return res.status(400).render("login", {
       pageTitle: "Login",
       errorMessage: "An account with this username does not exist",
@@ -81,7 +82,7 @@ export const logout = (req, res) => res.send("logout");
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "22b164dc77fd15726d7c",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
@@ -92,10 +93,74 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(url);
 };
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async (req, res) => {
   const {
     query: { code },
   } = req;
-  console.log(code);
-  return res.end();
+
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code,
+  };
+  const baseUrl = "https://github.com/login/oauth/access_token";
+
+  const params = new URLSearchParams(config);
+  const url = `${baseUrl}?${params.toString()}`;
+  const tokenRequest = await (
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  /* console.log(json); */
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userData);
+    const emailUrl = "/user/emails";
+    const emailData = await (
+      await fetch(`${apiUrl}${emailUrl}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
+    const emailObj = emailData.find((email) => email.primary && email.verified);
+    console.log(emailObj);
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+    } else {
+      const user = await User.create({
+        email: emailObj.email,
+        socialOnly: true,
+        username: userData.login,
+        name: userData.name ? userData.name : "Unknow",
+        location: userData.location ? userData.location : "Unknow",
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+    }
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
 };
